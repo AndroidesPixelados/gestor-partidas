2856c59e4e4c1e92ba782babeae859331c614f64
javimontes <lorathlon@gmail.com>
2013-04-04 13:40:31 +0200

    Añadidos los siguientes cambios:
    - Nuevo paquete es.androidespixelados.gestorpartida.dd4.adaptador
    - ExpandableListView
    - Adaptador relacionado con ella
    - Modificaciones en la vista (fragmento_contenido_derecho)

--- src/es/androidespixelados/gestorpartida/dd4/vista/DD4PartidaActivity.java
+++ src/es/androidespixelados/gestorpartida/dd4/vista/DD4PartidaActivity.java
@@ -1,10 +1,17 @@
 package es.androidespixelados.gestorpartida.dd4.vista;
 
+import java.util.ArrayList;
+
+import es.androidespixelados.gestorpartida.R;
+import es.androidespixelados.gestorpartida.dd4.adaptador.PlantillaAdaptador;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.TextView;
-import es.androidespixelados.gestorpartida.R;
 
 /**
  * Actividad de D&D 4Ed.
@@ -12,6 +19,10 @@ import es.androidespixelados.gestorpartida.R;
  *
  */
 public class DD4PartidaActivity extends FragmentActivity {
+
+	private ExpandableListView plantillas;
+	private TextView contenidoDerecho;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +30,37 @@ public class DD4PartidaActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.dd4_partida_main);
 		this.setTitle("Gestión de partida: WaterDeep");
+		
+		/** Obtengo el TextView donde se muestra el nombre de la partida **/
 		TextView lblNombrePartida = (TextView)findViewById(R.id.nombrePartida);
+		contenidoDerecho = (TextView)findViewById(R.id.contenidoDerecho);
+		
+		/**
+		 * Damos nombre a la partida. Posteriormente se hará de forma dinámica.
+		 */
 		lblNombrePartida.setText("WaterDeep");
+		
+		/** Obtengo la vista de la ExpandedListView **/
+		this.plantillas = (ExpandableListView)findViewById(R.id.plantillas);
+		/** Asigno el adaptador a la ExpandableListView **/
+		this.plantillas.setAdapter(new PlantillaAdaptador(this));
+		
+		
+		/** Listener sobre los diferentes botones de la ExpandableListView **/
+		this.plantillas.setOnChildClickListener(new OnChildClickListener() {
+			
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+				
+				/** Obtiene el nombre de la opción seleccionada **/
+				String opcionSeleccionada = (String)parent.getExpandableListAdapter().getChild(groupPosition, childPosition);
+				
+				/** Coloca el nombre de la opción seleccionada en el fragmento derecho **/
+				contenidoDerecho.setText(opcionSeleccionada);
+				
+				return false;
+			}
+		});
 		     
 	}
 	
