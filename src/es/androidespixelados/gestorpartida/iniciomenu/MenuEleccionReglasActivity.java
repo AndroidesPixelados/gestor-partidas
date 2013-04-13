ef185e272daf460a284d154001b1066114d27fd4
loic-prieto <loic.sephiroth@gmail.com>
2013-04-13 11:45:34 +0200

    Corrección de errores de acentos en comentarios e imports.

--- src/es/androidespixelados/gestorpartida/iniciomenu/MenuEleccionReglasActivity.java
+++ src/es/androidespixelados/gestorpartida/iniciomenu/MenuEleccionReglasActivity.java
@@ -1,59 +1,60 @@
 package es.androidespixelados.gestorpartida.iniciomenu;
 
-import es.androidespixelados.gestorpartida.R;
-import es.androidespixelados.gestorpartida.anima.vista.AnimaPartidaActivity;
-import es.androidespixelados.gestorpartida.dd4.vista.DD4PartidaActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import es.androidespixelados.gestorpartida.R;
+import es.androidespixelados.gestorpartida.anima.vista.AnimaPartidaActivity;
+import es.androidespixelados.gestorpartida.dd4.vista.DD4PartidaActivity;
 
 /**
- * Actividad para el menú de elección de reglas
+ * Actividad para el menï¿½ de elecciÃ³n de reglas
+ * 
  * @author Javi Montes
- *
+ * 
  */
 public class MenuEleccionReglasActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.menu_eleccion_reglas);
-		
-		/**Botones de elección de reglas**/
-		final Button btnAnima = (Button)findViewById(R.id.botonAnima);
-		final Button btnDungeons4 = (Button)findViewById(R.id.botonDungeons4);
-		/**Falta por implementar el botón de Vampiro**/
-		
+
+		/** Botones de elecciÃ³n de reglas **/
+		final Button btnAnima = (Button) findViewById(R.id.botonAnima);
+		final Button btnDungeons4 = (Button) findViewById(R.id.botonDungeons4);
+		/** Falta por implementar el botÃ³n de Vampiro **/
+
 		/**
-		 * Listener sobre el botón de Anima
+		 * Listener sobre el botÃ³n de Anima
 		 */
 		btnAnima.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				
+
 				Intent intent = new Intent(MenuEleccionReglasActivity.this, AnimaPartidaActivity.class);
-				
+
 				startActivity(intent);
-				
+
 			}
 		});
-		
+
 		/**
-		 * Listener sobre el botón de Dungeons 4
+		 * Listener sobre el botÃ³n de Dungeons 4
 		 */
 		btnDungeons4.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				
+
 				Intent intent = new Intent(MenuEleccionReglasActivity.this, DD4PartidaActivity.class);
-				
+
 				startActivity(intent);
-				
+
 			}
 		});
 	}
