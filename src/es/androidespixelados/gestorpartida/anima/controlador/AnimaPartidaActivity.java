1bd6bb5ed38f7af84f290b45dd5dd7c16e1aa14e
loic-prieto <loic.sephiroth@gmail.com>
2013-04-21 20:13:10 +0200

    integración con github

--- src/es/androidespixelados/gestorpartida/anima/controlador/AnimaPartidaActivity.java
+++ src/es/androidespixelados/gestorpartida/anima/controlador/AnimaPartidaActivity.java
@@ -3,39 +3,38 @@
  */
 package es.androidespixelados.gestorpartida.anima.controlador;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import roboguice.inject.InjectView;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import es.androidespixelados.gestorpartida.R;
 import es.androidespixelados.gestorpartida.controlador.ActividadBase;
 
 /**
- * Actividad para la gestión de la pantalla principal
+ * Actividad para la gestión de la pantalla principal de ánima.
  * 
- * @author loic
+ * @author Loïc Prieto.
  * 
  */
 public class AnimaPartidaActivity extends ActividadBase {
+
+	@InjectView(R.id.nombrePartida)
+	private TextView	lblNombrePartida;
+
+	// @Inject
+	// private ComponenteBDD componenteBDD;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.anima_partida_main);
 		this.setTitle("Gestión de partida: Vientos de LOL");
-		TextView lblNombrePartida = (TextView) findViewById(R.id.nombrePartida);
+
 		lblNombrePartida.setText("Vientos de LOL");
 
-		// Probamos la base de datos
-		SQLiteDatabase db = obtenerConexionABDD();
-		Cursor resultados = db.rawQuery("SELECT * FROM sistema;", null);
-		while (resultados.move(1)) {
-			int id = resultados.getInt(0);
-			String nombre = resultados.getString(1);
-
-			Log.i("pruebas", "Sistema " + id + ": " + nombre);
-		}
-		resultados.close();
-		db.close();
+		// Probamos la inyección de servicio
+		// Cursor c = componenteBDD.ejecutarConsulta("select * from partida", null);
+		// while (c.move(1)) {
+		// Log.d("[bdd]", "una fila encontrada, lol");
+		// }
 	}
 }
---
+++ src/es/androidespixelados/gestorpartida/util/ServicioI18N.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package es.androidespixelados.gestorpartida.util;
+
+
+/**
+ * Clase de utilidad para acceder a los recursos i18n fuera del contexto de una actividad.
+ * 
+ * @author Loïc Prieto
+ * 
+ */
+public class ServicioI18N {
+	public static String getTexto(String clave) {
+		return "";
+	}
+}
